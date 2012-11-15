659a67384ac3e0a096991029d11261910391a6fc
Matt Hernandez <matt@eventargs.com>
2012-11-15 13:22:27 -0500

    Added test to exercise setter.
    Reformatted tests a little so that the tests are grouped better within
    the modules and added a test to ensure that the value setter bug
    is/stays fixed.

--- test/lookout_test.js
+++ test/lookout_test.js
@@ -24,13 +24,17 @@
 
   module('Initialization');
 
-  test('initializes and is available', 8, function() {
+  test('initializes and is available', 4, function() {
     
     ok(window.lookout, 'lookout should be available');
     ok(window.disregard, 'disregard should be available');
 
     equal(window.lookout instanceof Function, true, 'lookout should be exposed as a function');
     equal(window.disregard instanceof Function, true, 'disregard should be exposed as a function');
+    
+  });
+  
+  test('does not throw', 4, function() {
     
     var obj = {};
     ok(window.lookout(obj, function() { }), 'lookout call with just an object and callback should not throw');
@@ -42,37 +46,41 @@
   
   module('events', {
     setup: function() {
-      this.obj = {
-        name: 'test object'
-      };
+      this.obj = { name: 'test object' };
     }
   });
   
-  test('lookout all properties', 1, function() {
-    var self = this;
+  test('setter updates value', 1, function() {
     
-    window.lookout(self.obj, function() {
-      ok(self.obj, 'callback should be called for all properties');
+    window.lookout(this.obj, 'name', function() {
+      equal(this.name, 'changed', 'should update the value of the property');
+    });
+    
+    this.obj.name = 'changed';
+  });
+  
+  test('lookout all properties', 1, function() {
+    
+    window.lookout(this.obj, function() {
+      ok(this, 'callback should be called for all properties');
     });
     
     this.obj.name = 'test all properties';
   });
   
   test('lookout single property', 1, function() {
-    var self = this;
     
-    window.lookout(self.obj, 'name', function() {
-      ok(self.obj, 'callback should be called for a single property');
+    window.lookout(this.obj, 'name', function() {
+      ok(this, 'callback should be called for a single property');
     });
     
     this.obj.name = 'test single property';
   });
   
   test('lookout array of properties', 1, function() {
-    var self = this;
     
-    window.lookout(self.obj, ['name'], function() {
-      ok(self.obj, 'callback should be called for an array of properties');
+    window.lookout(this.obj, ['name'], function() {
+      ok(this, 'callback should be called for an array of properties');
     });
     
     this.obj.name = 'test array of properties';
